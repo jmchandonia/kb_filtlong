0ad5f35fc5ee19260b40078a97d0b2a2bdba49cf
John-Marc Chandonia <JMChandonia@lbl.gov>
2022-03-08 22:29:40 +0000

    Copied some test code from kb_unicycler test, to upload long reads and filter them

--- test/kb_filtlong_server_test.py
+++ test/kb_filtlong_server_test.py
@@ -44,24 +44,174 @@ class kb_filtlongTest(unittest.TestCase):
         cls.callback_url = os.environ['SDK_CALLBACK_URL']
         suffix = int(time.time() * 1000)
         cls.wsName = "test_ContigFilter_" + str(suffix)
-        ret = cls.wsClient.create_workspace({'workspace': cls.wsName})  # noqa
+        print('created workspace ' + cls.getWsName())
+
+        cls.PROJECT_DIR = 'filtlong_outputs'
+        if not os.path.exists(cls.scratch):
+            os.makedirs(cls.scratch)
+        cls.prjdir = os.path.join(cls.scratch, cls.PROJECT_DIR)
+        if not os.path.exists(cls.prjdir):
+            os.makedirs(cls.prjdir)
+
+        cls.readUtilsImpl = ReadsUtils(cls.callbackURL, token=cls.token)
+        cls.dfuClient = DataFileUtil(url=cls.callbackURL, token=cls.token)
+        cls.staged = {}
+        cls.nodes_to_delete = []
+        cls.handles_to_delete = []
+        cls.setupTestData()
+        print('\n\n=============== Starting Filtlong tests ==================')
 
     @classmethod
     def tearDownClass(cls):
         if hasattr(cls, 'wsName'):
             cls.wsClient.delete_workspace({'workspace': cls.wsName})
             print('Test workspace was deleted')
+        if hasattr(cls, 'nodes_to_delete'):
+            for node in cls.nodes_to_delete:
+                cls.delete_shock_node(node)
+        if hasattr(cls, 'handles_to_delete'):
+            if cls.handles_to_delete:
+                cls.hs.delete_handles(cls.hs.hids_to_handles(cls.handles_to_delete))
+                print('Deleted handles ' + str(cls.handles_to_delete))
+
+    @classmethod
+    def getWsName(cls):
+        return cls.wsinfo[1]
+
+    def getImpl(self):
+        return self.serviceImpl
+
+    @classmethod
+    def delete_shock_node(cls, node_id):
+        header = {'Authorization': 'Oauth {0}'.format(cls.token)}
+        requests.delete(cls.shockURL + '/node/' + node_id, headers=header,
+                        allow_redirects=True)
+        print('Deleted shock node ' + node_id)
+
+    @classmethod
+    def upload_file_to_shock_and_get_handle(cls, test_file):
+        '''
+        Uploads the file in test_file to shock and returns the node and a
+        handle to the node.
+        '''
+        # file can't be in /kb/module/test or dfu won't find it
+        temp_file = os.path.join("/kb/module/work/tmp", os.path.basename(test_file))
+        shutil.copy(os.path.join("/kb/module/test", test_file), temp_file)
+
+        print('loading file to shock: ' + test_file)
+        fts = cls.dfuClient.file_to_shock({'file_path': temp_file,
+                                           'make_handle':True})
+
+        cls.nodes_to_delete.append(fts['shock_id'])
+        cls.handles_to_delete.append(fts['handle']['hid'])
+
+        return fts['shock_id'], fts['handle']['hid'], fts['size']
+
+    @classmethod
+    def upload_reads(cls, wsobjname, object_body, fwd_reads,
+                     rev_reads=None, single_end=False, sequencing_tech='Illumina',
+                     single_genome='1'):
+
+        ob = dict(object_body)  # copy
+        ob['sequencing_tech'] = sequencing_tech
+        ob['wsname'] = cls.getWsName()
+        ob['name'] = wsobjname
+        if single_end or rev_reads:
+            ob['interleaved'] = 0
+        else:
+            ob['interleaved'] = 1
+        print('\n===============staging data for object ' + wsobjname +
+              '================')
+        print('uploading forward reads file ' + fwd_reads['file'])
+        fwd_id, fwd_handle_id, fwd_size = \
+            cls.upload_file_to_shock_and_get_handle(fwd_reads['file'])
+
+        ob['fwd_id'] = fwd_id
+        rev_id = None
+        rev_handle_id = None
+        if rev_reads:
+            print('uploading reverse reads file ' + rev_reads['file'])
+            rev_id, rev_handle_id, rev_size = \
+                cls.upload_file_to_shock_and_get_handle(rev_reads['file'])
+            ob['rev_id'] = rev_id
+        obj_ref = cls.readUtilsImpl.upload_reads(ob)
+        objdata = cls.wsClient.get_object_info_new({
+            'objects': [{'ref': obj_ref['obj_ref']}]
+            })[0]
+        cls.staged[wsobjname] = {'info': objdata,
+                                 'ref': cls.make_ref(objdata),
+                                 'fwd_node_id': fwd_id,
+                                 'rev_node_id': rev_id,
+                                 'fwd_handle_id': fwd_handle_id,
+                                 'rev_handle_id': rev_handle_id
+                                 }
+
+    @classmethod
+    def setupTestData(cls):
+        print('Shock url ' + cls.shockURL)
+        # print('WS url ' + cls.wsClient.url)
+        # print('Handle service url ' + cls.hs.url)
+        print('CPUs detected ' + str(psutil.cpu_count()))
+        print('Available memory ' + str(psutil.virtual_memory().available))
+        print('staging data')
+
+        long_reads_high_depth = {'file': 'data/long_reads_high_depth.fastq.gz',
+                     'name': 'long_reads_high_depth.fastq.gz',
+                     'type': ''}
+        cls.upload_reads('shigella_long_high', {'single_genome': 1}, long_reads_high_depth,
+                         single_end=True, sequencing_tech="PacBio")
+        print('Data staged.')
+
+    @classmethod
+    def make_ref(self, object_info):
+        return str(object_info[6]) + '/' + str(object_info[0]) + \
+            '/' + str(object_info[4])
 
     # NOTE: According to Python unittest naming rules test method names should start from 'test'. # noqa
-    def test_your_method(self):
-        # Prepare test objects in workspace if needed using
-        # self.getWsClient().save_objects({'workspace': self.getWsName(),
-        #                                  'objects': []})
-        #
-        # Run your method by
-        # ret = self.getImpl().your_method(self.getContext(), parameters...)
-        #
-        # Check returned data with
-        # self.assertEqual(ret[...], ...) or other unittest methods
-        ret = self.serviceImpl.run_kb_filtlong(self.ctx, {'workspace_name': self.wsName,
-                                                             'parameter_1': 'Hello World!'})
+    def run_filtlong(self,
+                     long_reads_library = None,
+                     min_read_length = 1000,
+                     keep_percent = 90,
+                     target_bases = None):
+        params = {'workspace_name': self.getWsName(),
+                  'input_reads_library': long_reads_library,
+                  'output_reads_name': output_reads_name,
+                  'min_read_length': min_read_length,
+                  'keep_percent': keep_percent,
+                  'target_bases': target_bases
+                  }
+        
+        ret = self.serviceImpl.run_kb_filtlong(self.ctx, params)[0]
+        self.assertReportOK(ret, output_reads_name)
+
+    def assertReportOK(self, ret_obj, reads_name):
+        """
+        assertReportAssembly: given a report object, check the object existence
+        """
+        report = self.wsClient.get_objects2({
+                        'objects': [{'ref': ret_obj['report_ref']}]})['data'][0]
+        self.assertEqual('KBaseReport.Report', report['info'][2].split('-')[0])
+        self.assertEqual(1, len(report['data']['objects_created']))
+        self.assertEqual('Filtered reads',
+                         report['data']['objects_created'][0]['description'])
+        self.assertIn('Filtered ', report['data']['text_message'])
+        print("**************Report Message*************\n")
+        print(report['data']['text_message'])
+
+        reads_ref = report['data']['objects_created'][0]['ref']
+        reads = self.wsClient.get_objects([{'ref': reads_ref}])[0]
+
+        self.assertEqual('KBaseGenomeAnnotations.Assembly', assembly['info'][2].split('-')[0])
+        self.assertEqual(1, len(assembly['provenance']))
+        self.assertEqual(assembly_name, assembly['data']['assembly_id'])
+
+        temp_handle_info = self.hs.hids_to_handles([assembly['data']['fasta_handle_ref']])
+        assembly_fasta_node = temp_handle_info[0]['id']
+        self.nodes_to_delete.append(assembly_fasta_node)
+
+    # Uncomment to skip this test
+    # @unittest.skip("skipped test test_shigella_long_kbfile")
+    def test_shigella_long_kbfile(self):
+        self.run_filtlong( 'shigella_long_out',
+                           long_reads_library='shigella_long_high')
+        
